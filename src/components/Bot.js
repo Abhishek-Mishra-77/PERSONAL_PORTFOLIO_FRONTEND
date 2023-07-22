50270812ced3ad6278ab9b618cee1f3a9a70724d
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-07-22 10:46:54 +0530

    Done

---
+++ src/components/Bot.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Bot = () => {
+  return (
+    <div className="fixed bottom-5 sm:right-8 right-4 z-999 cursor-pointer text-blue
+    text-6xl bg-cyan-800 w-16 h-16 flex items-center justify-center rounded-full
+    animate-bounce" style={{ color: "cyan" }}>
+      <ion-icon name="chatbubble-ellipses"></ion-icon>
+    </div>
+
+  )
+}
+
+export default Bot
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        cyan: {
+          '50': '#e0f7ff',
+          '100': '#b3e6ff',
+          // ...
+          '600': '#0284c7',
+          // ...
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
